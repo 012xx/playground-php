793a705780dbfc6161c2e464e38e21641fd74425
012xx <night.fox.045@gmail.com>
2023-10-29 13:22:29 +0900

    Delete infra/mysql directory

--- infra/mysql/entrypoint/002-insert-users.sql
+++
@@ -1,8 +0,0 @@
-USE playgrounddb;
-
-INSERT INTO users 
-  (id, name, password_hash)
-VALUES 
-  (1, 'user01', 'wqwjqlwjqwlqjwl'),
-  (2, 'user02', ';fgsjfg;akg;k'),
-  (3, 'user03', 'bslbbnaafaeew');
--- infra/mysql/entrypoint/004-insert-tags.sql
+++
@@ -1,8 +0,0 @@
-USE playgrounddb;
-
-INSERT INTO tags
-  (id, name)
-VALUES
-  (1, 'テストタグ'),
-  (2, 'エイプリルドリーム'),
-  (3, 'Hello World');
