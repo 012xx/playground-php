d6e25afaad20b3a481b0d5c37a4390ac5544ea34
012xx <night.fox.045@gmail.com>
2023-11-06 12:28:37 +0000

    Create: index.php

---
+++ htdocs/public/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>Blog</title>
+    <meta name="description" content="テキストテキストテキストテキストテキストテキストテキストテキス">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="images/favicon.ico">
+    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+
+  <body>
+    <header id="header">
+    </header>
+
+    <div id="pickup" class="wrapper">
+      <article>
+        <images src="images/pickup1.jpg" alt="テキストテキストテキスト">
+        <h2 class="article-title">タイトルテキストテキストテキストテキストテキストテキストテキスト</h2>
+        <div class="readmore"><a href="#">READ MORE</a></div>
+      </article>
+
+      <article>
+        <images src="images/pickup2.jpg" alt="テキストテキストテキスト">
+        <h2 class="article-title">タイトルテキストテキストテキストテキストテキストテキストテキスト</h2>
+        <div class="readmore"><a href="#">READ MORE</a></div>
+      </article>
+
+      <article>
+        <images src="images/pickup3.jpg" alt="テキストテキストテキスト">
+        <h2 class="article-title">タイトルテキストテキストテキストテキストテキストテキストテキスト</h2>
+        <div class="readmore"><a href="#">READ MORE</a></div>
+      </article>
+      </main>
+  </body>
+</html>
