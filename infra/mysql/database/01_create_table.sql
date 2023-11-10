bdfa4d4337504e8bf61da9c28e110ea1df482375
012xx <night.fox.045@gmail.com>
2023-11-11 03:09:40 +0900

    Fix: SQL

--- infra/mysql/database/01_create_table.sql
+++ infra/mysql/database/01_create_table.sql
@@ -4,69 +4,69 @@ CREATE TABLE IF NOT EXISTS `User` (
     user_id UUID PRIMARY KEY NOT NULL UNIQUE,
     name VARCHAR(32) NOT NULL,
     password VARCHAR(255) NOT NULL
-
-    PRIMARY KEY (user_id)
 );
 
 CREATE TABLE IF NOT EXISTS `Article` (
     article_id UUID PRIMARY KEY NOT NULL UNIQUE,
     title VARCHAR(64) NOT NULL,
     context VARCHAR(20000) NOT NULL,
-    user_id UUID PRIMARY KEY NOT NULL UNIQUE,
-    created_at timestamp notnull notnull,
-    updated_at timestamp notnull notnull,
+    user_id UUID NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
 
     FOREIGN KEY (user_id)
-    REFERENCES users (id)
+    REFERENCES User (user_id)
     ON DELETE CASCADE
-)
+);
+
 
 CREATE TABLE IF NOT EXISTS `Image` (
-    article_id UUID PRIMARY KEY NOT NULL UNIQUE,
-    image_id UUID PRIMARY KEY NOT NULL UNIQUE,
+    article_id UUID NOT NULL,
+    image_id UUID NOT NULL UNIQUE,
     file_path VARCHAR(255) NOT NULL,
-    created_at timestamp notnull notnull,
-    updated_at timestamp notnull notnull,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
 
-    PRIMARY KEY (article_id, image_id)
-    FOREIGN KEY (article_id, image_id)
-    REFERENCES  Image(image_id)
+    PRIMARY KEY (article_id, image_id),
+    FOREIGN KEY (article_id)
     REFERENCES Article (article_id)
     ON DELETE CASCADE
-)
+);
+
 
 CREATE TABLE IF NOT EXISTS `Thumbnail` (
-    article_id UUID PRIMARY KEY NOT NULL UNIQUE,
-    image_id UUID PRIMARY KEY NOT NULL UNIQUE,
-    created_at timestamp notnull notnull,
-    updated_at timestamp notnull notnull,
+    article_id UUID NOT NULL,
+    image_id UUID NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
 
-    PRIMARY KEY (user_id, image_id)
-    FOREIGN KEY (user_id, image_id)
-    REFERENCES User (user_id)
-    REFERENCES Article (article_id)
+    PRIMARY KEY (article_id, image_id),
+    FOREIGN KEY (article_id)
+    REFERENCES Article (article_id),
+    FOREIGN KEY (image_id)
+    REFERENCES Image (image_id)
     ON DELETE CASCADE
-)
+);
 
 CREATE TABLE IF NOT EXISTS `Tag` (
     tag_id UUID PRIMARY KEY NOT NULL UNIQUE,
     name VARCHAR(32) NOT NULL,
-    created_at timestamp notnull notnull,
-    updated_at timestamp notnull notnull,
-
-    PRIMARY KEY (tag_id)
-)
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS `ArticleTag` (
-    article_id UUID PRIMARY KEY NOT NULL UNIQUE,
-    tag_id UUID PRIMARY KEY NOT NULL UNIQUE,
-    created_at timestamp notnull notnull,
-    updated_at timestamp notnull notnull,
+    article_id UUID NOT NULL,
+    tag_id UUID NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
 
-    PRIMARY KEY (article_id, tag_id)
-    FOREIGN KEY (article_id, tag_id)
-    REFERENCES Article (article_id)
+    PRIMARY KEY (article_id, tag_id),
+    FOREIGN KEY (article_id)
+    REFERENCES Article (article_id),
+    FOREIGN KEY (tag_id)
     REFERENCES Tag (tag_id)
     ON DELETE CASCADE
-)
+);
+
 
