41fc4ca85992dfdd1ae2da9a8d0863982c6f7335
012xx <night.fox.045@gmail.com>
2023-11-14 17:46:09 +0900

    Create: loginページ

--- htdocs/public/pages/login.php
+++ htdocs/public/pages/login.php
@@ -2,14 +2,14 @@
 <html lang="ja">
 <head>
 <meta charset="utf-8">
-<title>新規登録</title>
+<title>ログイン</title>
 <link rel="stylesheet" href="../css/global.css">
 <link rel="stylesheet" href="../css/signup-login.css">
 </head>
 <body>
 <div class="wrapper">
     <h2 class="welcome">playground-phpへようこそ！</h2>
-    <p class="text">新規登録して利用を開始しましょう。</p>
+    <p class="text">ログインして利用を開始しましょう。</p>
     <form method="post" >
         <div class="cp_iptxt">
             <label class="ef">
@@ -22,7 +22,9 @@
             </label>
         </div>
         <div class="button">
-            <button id="firstsignup">新規登録</button>
+            <button id="firstsignup">ログイン</button>
+            <br />
+            <a href="/pages/signup.php" class="link">新規登録はこちら</a>
         </div>
     </form>
 </div>
